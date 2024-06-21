1a90b85be50e647abece02e58d532eeeda9f136e
Sean Tuttle <sjt5_13@hotmail.com>
2024-06-20 22:42:18 -0500

    Add keybindings for LSP diagnostics

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,14 +1,20 @@
 require("nvchad.mappings")
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- easy bindings for clipboard use
 map("v", "<leader>cc", '"+y', { desc = "Copy to clipboard" })
 map({ "n", "v" }, "<leader>cp", '"+p', { desc = "Paste from clipboard" })
+
+-- make quitting an editor with lots of windows a little easier
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit editor" })
 map("n", "<leader>qf", "<cmd>qa!<cr>", { desc = "Force quit editor" })
+
+-- better support for LSP diagnostics
+map("n", "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+map("n", "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
+map("n", "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+map("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
