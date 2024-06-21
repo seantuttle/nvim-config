c236b19624b9309908f1930b64a63906d74b468c
Sean Tuttle <sjt5_13@hotmail.com>
2024-06-20 23:54:25 -0500

    Add nvim-surround plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -96,4 +96,12 @@ return {
 			vim.fn["mkdp#util#install"]()
 		end,
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
 }
