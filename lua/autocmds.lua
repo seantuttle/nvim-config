5a0608d3f0cf648ffcc79698d099a3b7be02d101
Sean Tuttle <sjt5_13@hotmail.com>
2024-06-23 14:54:54 -0500

    Have files open unfolded

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,13 +1,5 @@
 local cmd = vim.api.nvim_create_autocmd
 
--- cmd("LspAttach", {
---   group = vim.api.nvim_create_augroup("lsp", { clear = true }),
---   callback = function(args)
---     cmd("BufWritePre", {
---       buffer = args.buf,
---       callback = function()
---         vim.lsp.buf.format { async = false, id = args.data.client_id }
---       end,
---     })
---   end
--- })
+cmd("BufWinEnter", {
+	command = vim.cmd("silent! %foldopen!"),
+})
