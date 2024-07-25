19f219e8d3fc97bda47f6e8d9d0cfecc6806c274
Sean Tuttle <sjt5_13@hotmail.com>
2024-07-24 20:29:02 -0500

    Add mapping for word replacement with previously yanked word

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,16 +7,37 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 -- easy bindings for clipboard use
 map("v", "<leader>cc", '"+y', { desc = "Copy to clipboard" })
 map({ "n", "v" }, "<leader>cp", '"+p', { desc = "Paste from clipboard" })
+map("n", "<leader>rp", "ciw<C-r>0<ESC>", { desc = "Replace word by pasting previously yanked word" })
 
 -- make quitting an editor with lots of windows a little easier
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit editor" })
 map("n", "<leader>qf", "<cmd>qa!<cr>", { desc = "Force quit editor" })
 
 -- better support for LSP diagnostics
-map("n", "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
-map("n", "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
-map("n", "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
-map("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
+map(
+	"n",
+	"<leader>do",
+	"<cmd>lua vim.diagnostic.open_float()<CR>",
+	{ noremap = true, silent = true, desc = "Open diagnostic in floating box" }
+)
+map(
+	"n",
+	"<leader>d[",
+	"<cmd>lua vim.diagnostic.goto_prev()<CR>",
+	{ noremap = true, silent = true, desc = "Go to previous diagnostic" }
+)
+map(
+	"n",
+	"<leader>d]",
+	"<cmd>lua vim.diagnostic.goto_next()<CR>",
+	{ noremap = true, silent = true, desc = "Go to next diagnostic" }
+)
+map(
+	"n",
+	"<leader>dd",
+	"<cmd>Telescope diagnostics<CR>",
+	{ noremap = true, silent = true, desc = "Search diagnostics with Telescope" }
+)
 
 -- spell check mappings
 map({ "n", "v" }, "<leader>sc", "<cmd>set spell!<cr>", { desc = "Toggle spellcheck" })
